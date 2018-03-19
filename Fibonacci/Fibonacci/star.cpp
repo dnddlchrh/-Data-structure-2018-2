e2eb1c7ecb3f7e5c39e7e600e2dc184d7152e559
Park <Park@PARK-PC>
2018-03-19 22:24:26 +0900

    함수를 이용하여 별찍기

---
+++ Fibonacci/Fibonacci/star.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void star1(int i);
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		{
+			star1(i);
+		}
+		printf("\n");
+	}
+	
+}
+void star1(int i) {
+	for (int j = 0; j <= i+10; j++)
+	{
+		printf("*");
+	}
+}

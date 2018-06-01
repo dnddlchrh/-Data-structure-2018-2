1eb09abbfc27245ef7046212814b4fd0247435f8
dnddlchrh <37520426+dnddlchrh@users.noreply.github.com>
2018-06-01 14:27:35 +0900

    java0601

---
+++ java0601제네릭과 컬렉션,패키지/Java0601_6.java
@@ -0,0 +1,32 @@
+package java0601;
+
+import java.util.*;
+
+public class Java0601_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String,String> dic=new HashMap<String, String>();
+		
+		dic.put("baby", "아기");
+		dic.put("love", "사랑");
+		dic.put("apple", "사과");
+		Scanner sc=new Scanner(System.in);
+		while(true){
+			System.out.print("찾고 싶은 단어는?");
+			String eng=sc.next();
+			if(eng.equals("exit")){
+				System.out.println("종료합니다");
+				break;
+				
+			}
+			String kor=dic.get(eng);
+			if(kor==null)
+				System.out.println(eng+"는 없는 단어 입니다.");
+			else
+				System.out.println(kor);
+		}
+		sc.close();
+	}
+
+}
